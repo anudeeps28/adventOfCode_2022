7b4a4ccebb94a2775da5cb56e20fc22435a29ecb
anudeeps28 <anudeepsharma28@gmail.com>
2022-12-07 00:27:13 -0500

    day 3 - puzzle 2 SOLVED

--- day3.cpp
+++ day3.cpp
@@ -4,6 +4,7 @@
 #include<string>
 #include<algorithm>
 #include<numeric>
+#include<set>
 
 using namespace std;
 
@@ -21,57 +22,67 @@ int main() {
         data.push_back(line);
     }
 
+    vector<int> score;   
+    for (size_t i = 0; i < data.size(); i = i+3) {
 
-    vector<int> score = {};    
-    for (int i = 0; i < data.size(); i++) {
-        string word = data[i];
-        int half = word.size()/2;
+        vector<string> groupOfThree;
+
+        for (int k = 0; k < 3; k++) {
+            groupOfThree.push_back(data[k+i]);
+        }
+
         
-        vector<char> left = {};
-        vector<char> right = {};
+        string first = groupOfThree[0];
+        string second = groupOfThree[1];
+        string third = groupOfThree[2];
 
-        for (int j = 0; j < half; j++) {
-            left.push_back(word[j]);
+        char target;
+        vector<int> v1;
+        vector<int> v2;
+        vector<int> v3;
+        
+        for (size_t i = 0; i < first.size(); i++) {
+            v1.push_back(int(first[i]));
         }
 
-        for (int j = half; j < word.size(); j++) {
-            right.push_back(word[j]);
+        for (size_t j = 0; j < second.size(); j++) {
+            v2.push_back(int(second[j]));
         }
 
-        // for (auto i: left) {
-        //     cout << i << " ";
-        // }
+        for (size_t k = 0; k < third.size(); k++) {
+            v3.push_back(int(third[k]));
+        }
+            
+        for (size_t i = 0; i < v1.size(); i++) {
+            for (size_t j = 0; j < v2.size(); j++) {
+                for (size_t k = 0; k < v3.size(); k++) {
+                    if (v1[i] == v2[j] && v1[i] == v3[k]) {
+                        target = v1[i];
+                    }
 
-        char commonChar;
-        for (int i = 0; i < left.size(); i++) {
-            for (int j = 0; j < right.size(); j++) {
-                if (left[i] == right[j]) {
-                    commonChar = left[i];
                 }
             }
+
         }
+
+        // cout << target;
 
         int currentScore = 0;
-        if (122 >= int(commonChar) && int(commonChar) >= 97) {
-            currentScore += int(commonChar) - 96;
+        if (122 >= int(target) && int(target) >= 97) {
+            currentScore += int(target) - 96;
         }
 
-        if (65 <= int(commonChar) && int(commonChar) <= 90) {
-            currentScore += int(commonChar) - 38;
+        if (65 <= int(target) && int(target) <= 90) {
+            currentScore += int(target) - 38;
         }
 
-        cout << commonChar << " " << int(commonChar) << " " << currentScore << endl;
+        cout << target << " " << int(target) << " " << currentScore << endl;
         score.push_back(currentScore);
-        
-        
-        // cout << endl;
-        left.clear();
-        right.clear();
-
-    }
    
+    }
     int final = accumulate(score.begin(), score.end(), 0);
     cout << final;
 
+    
     
 }
